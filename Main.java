c9df694b68e2a4af29c8c2ef31493ad5bc22dd8e
Carlos Mario Puentes Diz <cmpuentes95@gmail.com>
2023-01-02 10:54:13 -0500

    Agregando comentario a la funsión suma en el método main

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        //OPeración suma
         Operaciones operaciones = new Operaciones();
         System.out.println(operaciones.suma(3, 4));
     }
