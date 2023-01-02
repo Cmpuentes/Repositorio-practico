333ac3431aabd92aad46b8e74d6d22e0fc2e57d6
Carlos Mario Puentes Diz <cmpuentes95@gmail.com>
2023-01-02 09:38:18 -0500

    Clase operaciones con la operación inicial suma

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Operaciones operaciones = new Operaciones();
+        System.out.println(operaciones.suma(3, 4));
+    }
+}
---
+++ Operaciones.java
@@ -0,0 +1,6 @@
+public class Operaciones {
+
+    public double suma(double a, double b){
+        return a + b;
+    }
+}
